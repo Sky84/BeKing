39730960596928e79d0595d5c11ca103199c2816
Etienne Salupo <etienne.salupocorvo@gmail.com>
2020-09-18 16:29:05 +0200

    feat init throw game + fix fmod always compiling

---
+++ Assets/ProjectileController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float timeBeforeRelease;
+    private bool isPressed;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        rb.isKinematic = true;
+        isPressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        rb.isKinematic = false;
+        isPressed = false;
+        StartCoroutine(ReleaseProjectile());
+    }
+
+    IEnumerator ReleaseProjectile()
+    {
+        yield return new WaitForSeconds(timeBeforeRelease);
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+}
