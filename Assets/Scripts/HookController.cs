b15b0346160b92320e0f57ca70e28a29b09d427e
Salupo Etienne <etienne.salupocorvo@gmail.com>
2020-09-19 18:37:08 +0200

    feat hook and projectile

---
+++ Assets/Scripts/HookController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookController : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public ProjectileController projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!projectile.isReleased)
+        {
+            lineRenderer.SetPosition(1, projectile.rb.position);
+        }
+        else if (projectile.nextProjectile)
+        {
+            projectile = projectile.nextProjectile.GetComponent<ProjectileController>();
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    public float timeBeforeRelease;
+    public float timeBeforeSpawn;
+    public float maxDistance;
+    public GameObject nextProjectile;
+    [HideInInspector] public bool isReleased;
+    private bool isPressed;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > maxDistance)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        rb.isKinematic = true;
+        isPressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        rb.isKinematic = false;
+        isPressed = false;
+        StartCoroutine(ReleaseProjectile());
+    }
+
+    IEnumerator ReleaseProjectile()
+    {
+        yield return new WaitForSeconds(timeBeforeRelease);
+        GetComponent<SpringJoint2D>().enabled = false;
+        isReleased = true;
+        yield return new WaitForSeconds(timeBeforeSpawn);
+        if (nextProjectile)
+        {
+            nextProjectile.SetActive(true);
+        }
+    }
+}
