b2247a9446fc9d061d262f6a45a16658735e4cda
Salupo Etienne <etienne.salupocorvo@gmail.com>
2020-09-28 09:15:26 +0200

    throw game init

---
+++ Assets/Scripts/ThrowGameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGameController : MonoBehaviour
+{
+    public BoxCollider2D cartCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
